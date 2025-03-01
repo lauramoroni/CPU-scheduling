2abbb621e2698ba83fbc22d556bc325c74fce2d5
lauramoroni <moronilaura28@gmail.com>
2025-03-01 15:28:46 -0300

    add file writing functionality

--- src/utils/FileHandler.java
+++ src/utils/FileHandler.java
@@ -1,5 +1,24 @@
 package utils;
 
-public class FileHandler {
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 
+import entities.Processes;
+import structures.stack.Stack;
+
+public class FileHandler {
+      public static void writeFile(String path, Stack<Processes> processes, String scheduler) throws Exception {
+         BufferedWriter writer = new BufferedWriter(new FileWriter(path));
+         
+         writer.write("Scheduler: " + scheduler);
+         writer.newLine();
+
+         while (!processes.isEmpty()) {
+             Processes process = processes.pop();
+             writer.write(process.toString());
+             writer.newLine();
+         }
+
+         writer.close();
+      }
 }
