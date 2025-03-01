7e66551d9e8678ec35f9892167bcba91cb6ca33d
lauramoroni <moronilaura28@gmail.com>
2025-03-01 15:34:35 -0300

    handle exceptions

--- src/utils/FileHandler.java
+++ src/utils/FileHandler.java
@@ -2,23 +2,27 @@ package utils;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
+import java.io.IOException;
 
 import entities.Processes;
 import structures.stack.Stack;
 
 public class FileHandler {
-      public static void writeFile(String path, Stack<Processes> processes, String scheduler) throws Exception {
-         BufferedWriter writer = new BufferedWriter(new FileWriter(path));
-         
-         writer.write("Scheduler: " + scheduler);
-         writer.newLine();
 
-         while (!processes.isEmpty()) {
-             Processes process = processes.pop();
-             writer.write(process.toString());
-             writer.newLine();
-         }
+    public static void writeFile(String path, Stack<Processes> processes, String scheduler) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(path))) {
+            writer.write("Scheduler: " + scheduler);
+            writer.newLine();
 
-         writer.close();
-      }
+            while (!processes.isEmpty()) {
+                Processes process = processes.pop();
+                writer.write(process.toString());
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            System.err.println("Error writing to file: " + e.getMessage());
+        } catch (Exception e) {
+            System.err.println("Error processing stack: " + e.getMessage());
+        }
+    }
 }
