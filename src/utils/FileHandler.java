2bb7198e104c403e40bc5026bed07eeea30bafab
lauramoroni <moronilaura28@gmail.com>
2025-03-17 14:10:19 -0300

    feat: implement read csv file

--- src/utils/FileHandler.java
+++ src/utils/FileHandler.java
@@ -10,25 +10,53 @@ public class FileHandler {
     public static Processes[] readFile(String path) {
         try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
             String line = null;
+
+            reader.mark(1000);
             int n = 0;
-            if ((line = reader.readLine()) != null) {
-                n = Integer.parseInt(line);
+            while ((line = reader.readLine()) != null) {
+                n++;
             }
-            Processes[] processes = new Processes[n]; // Implementando o vetor
+            reader.reset();
+
+            String regex = "";
+            if (path.endsWith(".txt")) {
+                regex = " ";
+            } else if (path.endsWith(".csv")) {
+                regex = ",";
+                n -= 1;
+                line = reader.readLine();
+                if (line == null) {
+                    System.err.println(Color.ANSI_RED + "Empty CSV file" + Color.ANSI_RESET);
+                    return null;
+                }
+            } else {
+                System.err.println(Color.ANSI_RED + "Invalid file format" + Color.ANSI_RESET);
+            }
+
+            Processes[] processes = new Processes[n]; 
+
             int index = 0;
             while ((line = reader.readLine()) != null) {
-                String[] data = line.split(" ");
+                String[] data = line.split(regex);
+
+                if (data.length != 5) {
+                    System.err.println(Color.ANSI_RED + "Invalid line format: " + line + Color.ANSI_RESET);
+                    continue; 
+                }
+                
                 int id = Integer.parseInt(data[0]);
                 String name = data[1];
                 int arrivalTime = Integer.parseInt(data[2]);
                 int burstTime = Integer.parseInt(data[3]);
                 int priority = Integer.parseInt(data[4]);
+
                 processes[index++] = new Processes(id, name, arrivalTime, burstTime, priority);
             }
 
             System.out.println(Color.ANSI_GREEN + "File read successfully!" + Color.ANSI_RESET);
 
             return processes;
+
         } catch (IOException e) {
             System.out.println(Color.ANSI_RED + "Error reading file: " + e.getMessage() + Color.ANSI_RESET);
             System.exit(1);
@@ -43,8 +71,6 @@ public class FileHandler {
             writeTxtFile(path, processes, scheduler, quantum);
         } else if (path.endsWith(".csv")) {
             writeCSVFile(path, processes, scheduler, quantum);
-        } else if (path.endsWith(".dat")) {
-            writeDatFile(path, processes, scheduler, quantum);
         } else {
             System.err.println(Color.ANSI_RED + "Invalid file format" + Color.ANSI_RESET);
         }
@@ -77,7 +103,7 @@ public class FileHandler {
 
     public static void writeCSVFile(String path, QueueList<Processes> processes, String scheduler, int quantum) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(path))) {
-            writer.write("ID,Name,Arrival Time,Burst Time,Priority,Quantum");
+            writer.write("PID,Name,Arrival Time,Burst Time,Priority,Quantum");
             writer.newLine();
 
             while (!processes.isEmpty()) {
@@ -90,30 +116,6 @@ public class FileHandler {
             }
         } catch (IOException e) {
             System.err.println(Color.ANSI_RED + "Error writing to file: " + e.getMessage() + Color.ANSI_RESET);
-        } catch (Exception e) {
-            System.err.println(Color.ANSI_RED + "Error processing queue: " + e.getMessage() + Color.ANSI_RESET);
-        }
-    }
-
-    public static void writeDatFile(String path, QueueList<Processes> processes, String scheduler, int quantum) {
-        try (FileOutputStream fileOut = new FileOutputStream(path);
-            ObjectOutputStream objectOut = new ObjectOutputStream(fileOut)) {
-
-            objectOut.writeUTF(scheduler); 
-            
-            if (scheduler.equals("Round Robin")) {
-                objectOut.writeInt(quantum);   
-            }
-
-            while (!processes.isEmpty()) {
-                Processes process = processes.remove(); 
-                objectOut.writeObject(process);
-
-                System.out.println(Color.ANSI_PURPLE + "Reading process " + process.getId() + Color.ANSI_RESET);
-                System.out.println(Color.ANSI_GREEN + "Process = " + process.getName() + " = written to binary file" + Color.ANSI_RESET);
-            }
-        } catch (IOException e) {
-            System.err.println(Color.ANSI_RED + "Error writing to binary file: " + e.getMessage() + Color.ANSI_RESET);
         } catch (Exception e) {
             System.err.println(Color.ANSI_RED + "Error processing queue: " + e.getMessage() + Color.ANSI_RESET);
         }
